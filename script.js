857c620d7879887e219571ccf421ed7b05c62f21
Vishal Prakash <v.r.prakash73@gmail.com>
2023-04-21 21:08:29 +1200

    reconfigured for wikipedia edit that removed title for button el and prettiered code

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 const url = new URL(window.location.href);
 
-if (url.origin.includes('wikipedia')) {
-    const buttons = document.getElementsByClassName("mw-ui-icon-element");
-    const spread = [...buttons];
-    const maxWidthButton = spread.filter(element => element.title === 'Toggle limited content width')[0];
-    maxWidthButton.click();
-    console.log('ayyy');
+if (url.origin.includes("wikipedia")) {
+  const buttons = document.getElementsByClassName("mw-ui-icon-element");
+  const spread = [...buttons];
+  const maxWidthButton = spread.filter(
+    (element) => element.tagName === "BUTTON"
+  )[0];
+  maxWidthButton.click();
+  console.log("ayyy");
 } else {
-    console.log("This ain't Wikipedia, bro");
-};
+  console.log("this ain't wikipedia bro");
+}
 
 completion(url);
