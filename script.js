95da242a358b3e82a0e7cbd995f28cb9a42a1542
Vishal Prakash <vishalprakash@Vishals-Macbook.local>
2024-06-13 17:41:19 +1200

    make the text smaller too

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 const url = new URL(window.location.href);
 
 if (url.origin.includes("wikipedia")) {
+  const smallTextBtn = document.getElementById("skin-client-pref-vector-feature-custom-font-size-value-0");
   const wideBtn = document.getElementById("skin-client-pref-vector-feature-limited-width-value-0");
   const appearanceBtn = document.getElementsByClassName("vector-pinnable-header-toggle-button vector-pinnable-header-unpin-button")[2];
+  smallTextBtn.click();
   wideBtn.click();
   appearanceBtn.click();
   console.log("ayyy");
