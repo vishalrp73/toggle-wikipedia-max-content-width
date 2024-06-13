afc6126f59b3e52eda6bd49e276d86b80a5574f3
Vishal Prakash <vishalprakash@Vishals-Macbook.local>
2024-06-13 17:33:22 +1200

    a year later they change their UI again

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 const url = new URL(window.location.href);
 
 if (url.origin.includes("wikipedia")) {
-  const maxWidthButton = document.getElementsByClassName(
-    "vector-limited-width-toggle"
-  )[0];
-  maxWidthButton.click();
+  const wideBtn = document.getElementById("skin-client-pref-vector-feature-limited-width-value-0");
+  const appearanceBtn = document.getElementsByClassName("vector-pinnable-header-toggle-button vector-pinnable-header-unpin-button")[2];
+  wideBtn.click();
+  appearanceBtn.click();
   console.log("ayyy");
 } else {
   console.log("this ain't wikipedia bro");
